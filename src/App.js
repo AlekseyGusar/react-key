ed27c109c4f5e07755021c148f8ce1ec159e0826
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-04-15 21:12:43 +0300

    render list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
 
-export default function App() {
-  return (
-    <div>
-      Some List:
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        { id: 1, text: 'Animals' },
+        { id: 2, text: 'Anime' },
+        { id: 3, text: 'Anti-Malware' },
+        { id: 4, text: 'Art Design' },
+        { id: 5, text: 'Books' },
+        { id: 6, text: 'Business' },
+        { id: 7, text: 'Calendar' },
+        { id: 8, text: 'Cloud Storage' },
+        { id: 9, text: 'File Sharing' },
+        { id: 10, text: 'Animals' },
+        { id: 11, text: 'Continuous Integration' },
+        { id: 12, text: 'Cryptocurrency' }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <ul> {this.state.list.map(item => <li key={item.id}> {item.text} </li>)}
+        </ul>
+      </div>
+    );
+  }
 }
+export default App;
